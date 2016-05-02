8272ce2cd20b1bfff405994b7645b8e2ce215701
susancarl <scarl2@earthlink.net>
2016-05-02 15:12:35 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -32,3 +32,17 @@ layer.setIcon(L.map.marker.icon)
                 "marker-symbol":"restaurant"
                 
                 map.fitBounds(featureLayer.getBounds());
+var dataFileToAdd = 'data/restaurants.geojson';
+var featureLayer = L.mapbox.featureLayer()
+    .loadURL(dataFileToAdd)
+    .addTo(map);
+featureLayer.on('ready', function() {
+  this.eachLayer(function(layer){
+    layer.setIcon(L.mapbox.marker.icon({
+      'marker-color': '#8834bb',
+      'marker-size': 'large',
+      'marker-symbol': 'restaurant'
+    }))
+  });
+  map.fitBounds(featureLayer.getBounds());
+});
