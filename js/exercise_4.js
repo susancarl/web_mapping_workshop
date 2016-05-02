92b5ddef0ee90644e852fefdd9e969f2fa7586f8
susancarl <scarl2@earthlink.net>
2016-05-02 14:40:47 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,7 +2,7 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.k6fj4l3f';
+var mapId = 'susancarl.019hp669';
 
 // And this is my access token, use yours.
 var accessToken = pk.eyJ1Ijoic3VzYW5jYXJsIiwiYSI6ImNpbnA5MTJheDEwYW51a20zYWpkNjZ5eWYifQ.P9V_ZZj0a5mowJe3nRHoMQ
